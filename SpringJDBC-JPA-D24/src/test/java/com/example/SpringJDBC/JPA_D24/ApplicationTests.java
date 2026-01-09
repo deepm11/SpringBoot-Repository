c4643e157b292d52da28cf5e651d11df8f82f486
Deepm5 <127013218+deepm11@users.noreply.github.com>
2026-01-09 17:53:28 +0530

    Add files via upload

---
+++ SpringJDBC-JPA-D24/src/test/java/com/example/SpringJDBC/JPA_D24/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringJDBC.JPA_D24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
